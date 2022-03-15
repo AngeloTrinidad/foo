1fa79490b6f0a4ceb59ddbf3d92c816ffc0f1e1d
Mariza <72386277+MarizaKiram@users.noreply.github.com>
2022-03-15 08:20:38 +0800

    Add files via upload

---
+++ member1.php/number.php
@@ -0,0 +1,14 @@
+
+    <?php
+    $number = 10;
+    if($number >= 0){
+echo ("The " . $number . "is a positive number.");
+
+
+    }elseif ($number < 0){
+        echo ("The " . $number ." is a negative number.");
+        }else{
+            echo($number . "is not a number");
+        }
+       }
+    ?> 
